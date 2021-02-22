c296c8998ffa91afff834c1d2e6e1d060d0e3b34
Nova+ <cszonalt@gmail.com>
2021-02-22 17:42:11 +0200

    v1.0 map update (+now it's tiled), new positions handling, houses markers added

---
+++ _tools/bundle_files.js
@@ -0,0 +1,105 @@
+const gulp       =  require('gulp');
+const concat     =  require('gulp-concat');
+// const sourcemaps =  require('gulp-sourcemaps');
+const minify =      require('gulp-minify');
+const cleancss =    require('gulp-clean-css');
+const htmlmin =     require('gulp-htmlmin');
+
+const fs = require("fs");
+
+(async () => {
+  console.log("==================");
+  const time_now = Date.now();
+  
+  //JS
+  let js_file_saved = false
+  const new_js_file_name = `bundle-js-${time_now}.js`;
+  const new_js_file_name_minified = `bundle-js-${time_now}-min.js`;
+  await new Promise(resolve => {
+    // gulp.src(['../dev/js/*.js', '!../dev/js/debug.js'])
+    gulp.src([
+      "../dev/js/Leaflet.ContinuousZoom.js",
+      "../dev/js/misc.js",
+      "../dev/js/main.js",
+      "../dev/js/hud.js",
+      "../dev/js/plot.js",
+      "../dev/js/servers_icons.js",
+      "../dev/js/markers.js",
+      "../dev/js/servers.js"
+    ])
+    // .pipe(sourcemaps.init())
+    .pipe(concat(new_js_file_name))
+    .pipe(minify())
+    // .pipe(sourcemaps.write())
+    .pipe(
+        gulp.dest('../').on('end', ()=> {
+            js_file_saved = true;
+            console.log("js minified => " +  new_js_file_name);
+            resolve();
+        })
+    );
+  })
+
+  if(!js_file_saved) return console.log("js file wasnt saved");
+
+  //CSS
+
+  const new_css_file_name = `bundle-css-${time_now}.css`;
+  await new Promise(resolve => {
+    gulp.src([
+      "../dev/style-dev.css"
+    ])
+    .pipe(concat(new_css_file_name))
+    .pipe(cleancss())
+    .pipe(
+        gulp.dest('../').on('end', ()=> {
+          console.log("css minified => " + new_css_file_name);
+          resolve()
+        })
+    );
+  })
+
+  //HTML
+
+  const html = fs.readFileSync("../dev/index-dev.html", "utf-8")
+    .replace(`<link rel="stylesheet" href="style-dev.css">`, `<link rel="stylesheet" href="${new_css_file_name}">`)
+    .replace(/<body[^>]*>((.|[\n\r])*)<\/body>/gm, `<body>
+    <noscript>You need to enable JavaScript to run this app.</noscript>
+    <script>const fileUpdatedAt = ${Date.now()}, is_dev_environment = false;</script>
+    <script src="${new_js_file_name_minified}"></script>
+  </body>`);
+
+  fs.writeFileSync("../index.html", html);
+  console.log("html converted => index.html");
+
+  await new Promise(resolve => {
+    gulp.src([
+      "../index.html"
+    ])
+    .pipe(concat("index.html"))
+    .pipe(htmlmin({ collapseWhitespace: true }))
+    .pipe(
+        gulp.dest('../').on('end', ()=> {
+          console.log("html minified => index.html");
+          resolve()
+        })
+    );
+  })
+
+  console.log("==================");
+
+  //FILE CLEANUP
+  fs.readdirSync("../").map(file=>{
+    // if(file.startsWith("bundle-js-") && (file !== new_js_file_name && file !== new_js_file_name_minified)){
+    if(file.startsWith("bundle-js-") && file !== new_js_file_name_minified){
+      fs.unlink("../" + file, () => {
+        console.log(file + " file deleted")
+      })
+    }else if(file.startsWith("bundle-css-") && file !== new_css_file_name){
+      fs.unlink("../" + file, () => {
+        console.log(file + " file deleted")
+      })
+    }
+  })
+
+})();
---
+++ _tools/rename_tiles.js
@@ -0,0 +1,51 @@
+const fs = require("fs");
+// for photoshop adapting the tiles
+
+// const imagesLocatedAt = "../images/maps/tiles/color/";
+const imagesLocatedAt = "../images/maps/dark/images/"; //must end with "/"
+const moveToFolderAt = "../images/maps/dark-mode-tiles/";
+
+const tileSize = 288;
+const suffix = ".jpg";
+const startResolution = 9216;
+const maxZoom = 7;
+const minZoom = 2;
+
+if(!fs.existsSync(imagesLocatedAt)){
+    return console.log("folder/files do not exists?")
+}
+const total_files = fs.readdirSync(imagesLocatedAt).length;
+
+for (let    index = maxZoom, resolution = startResolution; 
+            index > minZoom; 
+            index--, resolution *= 0.5) {
+
+    console.log(index, resolution)
+    convert_names("" + index + "_", resolution);
+}
+
+try{
+    fs.renameSync(`${imagesLocatedAt}${minZoom}${suffix}`, `${imagesLocatedAt}${minZoom}_0_0${suffix}`);
+}catch(err){}
+
+if(moveToFolderAt){
+    fs.renameSync(imagesLocatedAt, moveToFolderAt);
+}
+
+
+function convert_names(prefix = "7_", resolution = 9216){
+    const column_count = resolution / tileSize;
+    for (let i = 0, row = 0; i < total_files; i++) {
+        const column = (i % (column_count));
+        let old_file_name = prefix + (i < 9 ? "0" + (i + 1) : "" + (i + 1)) + suffix;
+
+        console.log(`${imagesLocatedAt}${old_file_name}`, `${imagesLocatedAt}${prefix}${column}_${row}${suffix}`);
+        try{
+            fs.renameSync(`${imagesLocatedAt}${old_file_name}`, `${imagesLocatedAt}${prefix}${column}_${row}${suffix}`);
+        }catch(err){}
+
+        if(column === column_count - 1){
+            row++;
+        }
+    }
+}
