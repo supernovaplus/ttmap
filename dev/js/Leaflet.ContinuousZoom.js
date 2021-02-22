93ed5f459e4d2677b7bcdf6b6a6751dad4379017
Nova+ <supernovaplus@users.noreply.github.com>
2021-02-22 17:45:31 +0200

    v1.0 map update (+now it's tiled), new positions handling, houses markers added

---
+++ dev/js/Leaflet.ContinuousZoom.js
@@ -0,0 +1,67 @@
+// Fill in missing zoom levels of a tile layer with scaled previous zooms.
+// Written by Ilya Zverev, licensed WTFPL
+
+L.TileLayer.mergeOptions({
+	nativeZooms: [] // array of integers
+});
+
+L.TileLayer.addInitHook(function () {
+	const opt = this.options,
+	    zooms = opt.nativeZooms;
+	if( zooms && zooms.length > 0 ) {
+		let minZoom = 100, i;
+		for (i = 0; i < zooms.length; i++)
+			if (zooms[i] < minZoom)
+				minZoom = zooms[i];
+		opt.minZoom = Math.max(opt.minZoom, minZoom - 1);
+	}
+});
+
+L.TileLayer.include({
+	getTileSize: function () {
+		const map = this._map,
+		tileSize = L.GridLayer.prototype.getTileSize.call(this),
+		zoom = this._tileZoom + this.options.zoomOffset,
+		nativeZoom = this._mapNativeZoom(zoom);
+
+		return nativeZoom == zoom ? tileSize :
+			tileSize.divideBy(map.getZoomScale(nativeZoom, zoom)).round();
+	},
+
+	_getZoomForUrl: function () {
+		let zoom = this._tileZoom,
+		maxZoom = this.options.maxZoom,
+		zoomReverse = this.options.zoomReverse,
+		zoomOffset = this.options.zoomOffset;
+
+		if (zoomReverse) {
+			zoom = maxZoom - zoom;
+		}
+
+		zoom += zoomOffset;
+
+		return this._mapNativeZoom(zoom);
+	},
+
+	_mapNativeZoom: function (zoom) {
+		let zooms = this.options.nativeZooms,
+		minNativeZoom = this.options.minNativeZoom,
+		maxNativeZoom = this.options.maxNativeZoom;
+
+		if (zooms && zooms.length > 0) {
+			let prevZoom = -1, minZoom = 100, i;
+			for (i = 0; i < zooms.length; i++) {
+				if( zooms[i] <= zoom && zooms[i] > prevZoom )
+					prevZoom = zooms[i];
+				if( zooms[i] < minZoom )
+					minZoom = zooms[i];
+			}
+			zoom = prevZoom < 0 ? minZoom : prevZoom;
+		} else if (maxNativeZoom !== null && zoom > maxNativeZoom) {
+			zoom = maxNativeZoom;
+		} else if (minNativeZoom !== null && zoom < minNativeZoom) {
+			zoom = minNativeZoom;
+		}
+		return zoom;
+	}
+});
---
+++ dev/js/main.js
@@ -0,0 +1,128 @@
+
+const base_folder = is_dev_environment ? "../" : "./";
+const emoji_folder = base_folder + "images/openmoji/";
+const map_folder = base_folder + "images/maps/";
+const company_emoji_folder = base_folder + "images/companyemoji/";
+const custom_emoji_folder = base_folder + "images/custom/";
+
+const is_mobile_device = (typeof window.orientation !== "undefined") || (navigator.userAgent.indexOf('IEMobile') !== -1);
+let copy_link_url = window.location.protocol + "//" + window.location.host + "/";
+
+const params = {
+    "hideplayers": new URL(location.href).searchParams.get("hideplayers") === "",
+    "hideicons": new URL(location.href).searchParams.get("hideicons") === "",
+    //"plot_url": new URL(location.href).searchParams.get("plot_url") || false,
+    "coords": (()=>{
+        const paramx = new URL(location.href).searchParams.get("x");
+        const paramy = new URL(location.href).searchParams.get("y");
+        return !paramx || !paramy ? undefined : [paramx, paramy];
+    })()
+};
+
+if(localStorage.getItem("ttmap_options_1")){
+    localStorage.removeItem("ttmap_options_1");
+}
+
+// if(params.plot_url){
+//     params.hideplayers = true;
+// }
+
+const options = {
+    current_map_index: 1,
+    sidebar_open: true,
+    current_trail_index: is_mobile_device ? 3 : 2,
+    timestamp: 0,
+
+    markers: {
+        "business": true,
+        "garages": true,
+        "self_storage": true,
+        "houses" : true
+    }
+};
+
+let currentTileLayer = null;
+
+const save_options = () => {
+    options.timestamp = Date.now();
+    localStorage.setItem("ttmap_options_2", JSON.stringify(options));
+};
+
+try{
+    const options_from_storage =  localStorage.getItem("ttmap_options_2");
+    if(options_from_storage){
+        Object.assign(options, JSON.parse(options_from_storage));
+
+        if(params.hideicons){
+            for (const key in options.markers) {
+                options.markers[key] = false;
+            }
+        }
+    }
+    save_options();
+}catch(err){
+    console.error(err);
+};
+
+const div_map = cel("div", {id: "map"});
+document.body.appendChild(div_map);
+
+div_map.style.height = window.innerHeight + "px";
+window.onresize = () => {
+    div_map.style.height = window.innerHeight + "px";
+};
+
+L.CRS.Kebab = L.extend({}, L.CRS.Simple, {
+    projection: {
+        project: latlng => new L.Point(latlng.lat, latlng.lng), //reverse lat/lng for easier passing of x/y
+        unproject: point => new L.LatLng(point.x, point.y),
+        bounds: new L.Bounds([-180, -90], [180, 90])
+    },
+    transformation: new L.Transformation(
+      0.005175, //scale-x
+      34.38, //shift-x
+      -0.005173, //scale-y
+      46.79355), //shift-y
+});
+
+const map = L.map('map', {
+    renderer: L.canvas(),
+    zoomControl: false,
+    zooms: [2,3,4,5,6,7,8,9],
+    minZoom : 2,
+    maxZoom : 9,
+    // zoomSnap: 1,
+    fadeAnimation: true,
+    zoomAnimation: true,
+    crs: L.CRS.Kebab
+}).setView([0,0], 5);
+
+
+const map_list = [
+    {
+        name: "Color Mode",
+        bgcolor: "#0fa8d1",
+        tileLayer: L.tileLayer(base_folder + "images/maps/color-mode-tiles/{z}_{x}_{y}.jpg",{
+            tileSize: 288,
+            nativeZooms: [3,4,5,6,7],
+            noWrap: true,
+            bounds: [{ lat: -6566, lng: -4735 }, { lat: 7166, lng: 8906 }],
+            reuseTiles : true
+        })
+    },
+    { 
+        name: "Dark Mode", 
+        bgcolor: "#171717",
+        tileLayer: L.tileLayer(base_folder + "images/maps/dark-mode-tiles/{z}_{x}_{y}.jpg",{
+            tileSize: 288,
+            nativeZooms: [3,4,5,6,7],
+            noWrap: true,
+            bounds: [{ lat: -6566, lng: -4735 }, { lat: 7166, lng: 8906 }],
+            reuseTiles : true
+        })
+    },
+];
+
+L.control.zoom({
+    position: 'topright'
+}).addTo(map);
